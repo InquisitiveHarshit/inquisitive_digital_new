ebc196321fbffaeebd8fe3472dc43116e67e5173
Harshit Gaur <harshitgaur737@gmail.com>
2026-07-27 14:37:57 +0530

    tawk integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -115,6 +115,19 @@ export default function RootLayout({
           <div className="noise-overlay" />
           {children}
           <AdminExclusionWrapper>
+            <Script id="tawk-to" strategy="lazyOnload">
+              {`
+                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/6a671e63e694901d4cd0678d/1juhctdfh';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+                })();
+              `}
+            </Script>
             <Contact />
             <Footer />
           </AdminExclusionWrapper>
